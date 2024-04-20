d2d1eda522fa7ca87d8135e2264afe5f44c4fc8d
byte.nibble <byte.nibble@DESKTOP-V716SHR>
2024-04-20 14:35:03 -0300

    Add project files.

---
+++ Myter Engine CMake.cpp
@@ -0,0 +1,12 @@
+﻿// Myter Engine CMake.cpp : Defines the entry point for the application.
+//
+
+#include "Myter Engine CMake.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
